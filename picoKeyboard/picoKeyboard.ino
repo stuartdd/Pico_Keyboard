e89a2c6da8e4c7a482d5310042b4dce87f5308b5
stuartdd <stuartdd@gmail.com>
2025-04-07 16:25:50 +0100

    config

--- picoKeyboard/picoKeyboard.ino
+++ picoKeyboard/picoKeyboard.ino
@@ -21,8 +21,13 @@
 #define SCREEN_HEIGHT 64     // OLED display height, in pixels
 #define SCREEN_LINE 16
 #define SCREEN_CHAR 10
+#define LOG_LINES 6
 #define QUAD_W 4
 #define QUAD_H 6
+#define CHAR_MAXIMUM 127  // Ignore chars above 126
+#define CHAR_CR 10        // END OF LINE
+#define CHAR_SPACE 32     // SPACE
+#define CHAR_DELIM 124    // |
 
 #define LIVE_LED 22  // Actual Arduino pin number
 
@@ -48,6 +53,8 @@
 // Menu height is 1 + 3 for large display, 7 for small as yellow is always 2 lines
 #define MENU_HEIGHT_LARGE 4
 #define MENU_HEIGHT_SMALL 7
+#define MENU_LINES 10
+#define MENU_COLUMNS 10
 
 #define MODE_SETUP 0
 #define MODE_MENU 1
@@ -80,6 +87,15 @@ int buttonBits = 0;
 int tos = 0;
 int oldTos = 0;
 volatile bool updateScreen = true;  // Screen needs to be redrawn
+String logSubjectStr = "";
+String logLines[LOG_LINES] = { "", "", "", "", "", "" };
+int logLinePos = 0;
+char numberArray[20];
+
+
+
+char menuLines[MENU_LINES][MENU_COLUMNS + 1];
+int menuCount = 0;
 
 volatile bool driveConnected = false;
 
@@ -88,17 +104,118 @@ void printDiag(String s) {
     Serial.println(s);
   }
 }
+
+void logLine(String s, int d) {
+  logLines[logLinePos] = s;
+  display.clearDisplay();
+  display.setRotation(screenRotation);
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.println(logSubjectStr);
+  display.setTextSize(1);
+  int lp = logLinePos;
+  for (int i = 0; i < LOG_LINES; i++) {
+    lp++;
+    if (lp >= LOG_LINES) {
+      lp = 0;
+    }
+    display.println(logLines[lp]);
+  }
+  display.display();
+  logLinePos++;
+  if (logLinePos >= LOG_LINES) {
+    logLinePos = 0;
+  }
+  delay(d);
+}
+
+void logSubject(String s) {
+  logSubjectStr = s;
+  logLinePos = 0;
+  for (int i = 0; i < LOG_LINES; i++) {
+    logLines[i] = "";
+  }
+  display.clearDisplay();
+  display.setRotation(screenRotation);
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println(logSubjectStr);
+  display.display();
+}
+
+int readKeyFile(File f) {
+  for (int x = 0; x < MENU_LINES; x++) {
+    for (int y = 0; y < (MENU_COLUMNS + 1); y++) {
+      menuLines[x][y] = 0;
+    }
+  }
+  int row = 0;
+  int coll = 0;
+  int char1 = 0;
+  int capture = 0;
+  while (f.available()) {
+    char1 = f.read();
+    if (char1 == CHAR_CR) {
+      row++;
+      if (row >= MENU_LINES) {
+        return MENU_LINES;
+      }
+      coll = 0;
+      capture = 0;
+    } else {
+      if (char1 == CHAR_DELIM) {
+        capture++;
+      } 
+      if ((char1 >= CHAR_SPACE) && (char1 < CHAR_MAXIMUM) && (coll < MENU_COLUMNS) && ((capture == 0) || (capture == 2))) {
+        menuLines[row][coll] = char1;
+        coll++;
+        capture = 0;
+      }
+    }
+  }
+  return row;
+}
+
+void loadKeyData() {
+  menuCount = 0;
+  logSubject("Loading:");
+  logLine("FS Init", 10);
+  if (FatFS.begin()) {
+    logLine("FS OK", 10);
+  } else {
+    logLine("FS FAIL", 5000);
+    return;
+  }
+  Dir dir = FatFS.openDir("/");
+  while (true) {
+    if (!dir.next()) {
+      break;
+    }
+    if (dir.isFile()) {
+      logLine(dir.fileName(), 10);
+      File f = dir.openFile("r");
+      if (f) {
+        logLine("FS openFile OK", 10);
+        menuCount = readKeyFile(f);
+        logLine("FS openFile done", 10);
+        logLine(itoa(menuCount, numberArray, 10), 11);
+      } else {
+        logLine("FS openFile FAIL", 5000);
+        return;
+      }
+    }
+  }
+}
+
 void unplug(uint32_t i) {
   (void)i;
   driveConnected = false;
-  Serial.println("unplug");
 }
 
 // Called by FatFSUSB when the drive is mounted by the PC.  Have to stop FatFS, since the drive data can change, note it, and continue.
 void plug(uint32_t i) {
   (void)i;
   driveConnected = true;
-  Serial.println("plug");
 }
 
 // Called by FatFSUSB to determine if it is safe to let the PC mount the USB drive.  If we're accessing the FS in any way, have any Files open, etc. then it's not safe to let the PC mount the drive.
@@ -107,26 +224,14 @@ bool mountable(uint32_t i) {
   return true;
 }
 
-struct option {
-  String disp;
-  String keys;
-};
-
-// Number of options. Not display specific
-option options[] = {
-  { "Opt 1", "A" },
-  { "Opt 2", "B" },
-  { "Opt 3", "C" },
-  { "Opt 4", "D" },
-  { "Opt 5", "E" },
-  { "Opt 6", "F" },
-  { "Opt 7", "G" },
-  { "Opt 8", "H" },
-  { "Opt 9", "I" }
-};
-
-int optionCount = sizeof(options) / sizeof(option);
-
+void waitForButtonC() {
+  while (digitalRead(IN_PIN_C) == HIGH) {
+    delay(10);
+  }
+  while (digitalRead(IN_PIN_C) == LOW) {
+    delay(10);
+  }
+}
 
 bool scanButtons() {
   int bb = 0;
@@ -196,9 +301,11 @@ void box(int lin, int quad) {
   }
 }
 
-void resetScreen(int showButtons) {
+void clearScreen(int showButtons) {
   display.clearDisplay();
   display.setRotation(screenRotation);
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setTextWrap(false);
   display.setCursor(0, 0);
   display.setTextSize(2);
   if ((showButtons & BIT_1) == BIT_1) {
@@ -217,7 +324,7 @@ void resetScreen(int showButtons) {
 
 void updateScreenPgm() {
   updateScreen = false;
-  resetScreen(BIT_2 | BIT_4 | BIT_8);
+  clearScreen(BIT_2 | BIT_4 | BIT_8);
   display.println("Program:");
   display.println(" Ident");
   display.println(" Upload");
@@ -227,7 +334,7 @@ void updateScreenPgm() {
 
 void updateScreenMenu() {
   updateScreen = false;
-  resetScreen(BIT_ALL);
+  clearScreen(BIT_ALL);
   display.println(" Reset");
   display.println(" Size");
   display.println(" Rotate");
@@ -237,24 +344,24 @@ void updateScreenMenu() {
 
 void updateScreenHid() {
   updateScreen = false;
-  resetScreen(BIT_NONE);
+  clearScreen(BIT_NONE);
   menuLine = tos;
-  display.println(options[menuLine].disp);
+  display.println(menuLines[menuLine]);
   if (menuMax) {
     display.setTextSize(1);
     menuHeight = MENU_HEIGHT_SMALL;
   } else {
     menuHeight = MENU_HEIGHT_LARGE;
   }
-  if (menuHeight > optionCount) {
-    menuHeight = optionCount;
+  if (menuHeight > menuCount) {
+    menuHeight = menuCount;
   }
   for (int i = 1; i < menuHeight; i++) {
     menuLine++;
-    if (menuLine >= optionCount) {
+    if (menuLine >= menuCount) {
       menuLine = 0;
     }
-    display.println(options[menuLine].disp);
+    display.println(menuLines[menuLine]);
   }
   display.display();
 }
@@ -269,12 +376,10 @@ void sendKeys(String ch) {
 }
 
 void sendButtonPressed() {
-  resetScreen(BIT_NONE);
-  display.println("Sending...");
-  display.println(options[tos].disp);
-  display.display();
-  sendKeys(options[tos].keys);
-  delay(1000);
+  logSubject("Sending...");
+  logLine(menuLines[tos], 100);
+  sendKeys(menuLines[tos]);
+  logLine("Done", 1000);
   updateScreen = true;
 }
 
@@ -289,7 +394,7 @@ void setMode(int newMode) {
 void upButtonPressed() {
   oldTos = tos;
   tos++;
-  if (tos >= optionCount) {
+  if (tos >= menuCount) {
     tos = 0;
   }
   if (oldTos != tos) {
@@ -301,7 +406,7 @@ void downButtonPressed() {
   oldTos = tos;
   tos--;
   if (tos < 0) {
-    tos = optionCount - 1;
+    tos = menuCount - 1;
   }
   if (oldTos != tos) {
     updateScreen = true;
@@ -309,9 +414,7 @@ void downButtonPressed() {
 }
 
 void reset() {
-  resetScreen(BIT_NONE);
-  display.println("Reset...");
-  display.display();
+  logSubject("Resetting:");
   delay(1000);
   rp2040.reboot();
 }
@@ -326,17 +429,13 @@ void rotateDisplay() {
 }
 
 void setLines() {
-  resetScreen(BIT_NONE);
   menuMax = !menuMax;
-  display.println("ZOOM");
+  logSubject("ZOOM:");
   if (menuMax) {
-    display.println("...8 Lines");
+    logLine("...8 Lines", 1000);
   } else {
-    display.println("...4 Lines");
+    logLine("...4 Lines", 1000);
   }
-  display.display();
-  delay(1000);
-  // Normal 1:1 pixel scale
   setMode(MODE_HID);
 }
 
@@ -365,49 +464,52 @@ void setup() {
   pinMode(IN_PIN_D, INPUT_PULLUP);
   pinMode(IN_PIN_C, INPUT_PULLUP);
   pinMode(LIVE_LED, OUTPUT);
-
-
-  digitalWrite(LIVE_LED, HIGH);
-  if (programButtons()) {
-    digitalWrite(LIVE_LED, LOW);
-    Serial.begin(9600);
-    while (!Serial) {
-      delay(1);
-    }
-    delay(5000);
-    if (!FatFS.begin()) {
-      Serial.println("FatFS initialization failed!");
-      while (1) {
-        delay(1);
-      }
-    }
-    Serial.println("FatFS initialization done.");
-    FatFSUSB.onUnplug(unplug);
-    FatFSUSB.onPlug(plug);
-    FatFSUSB.driveReady(mountable);
-    // Start FatFS USB drive mode
-    FatFSUSB.begin();
-    Serial.println("FatFSUSB started.");
-
-    while (programButtons()) {
-      delay(500);
-    }
-    setMode(MODE_PGM);
-  } else {
-    setMode(MODE_HID);
-  }
-
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     errorCode(4);
   }
   // Clear the buffer
-  resetScreen(BIT_NONE);
-  display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.println("Setup...");
-  display.display();
+  clearScreen(BIT_NONE);
+  logSubject("Setup:");
   digitalWrite(LIVE_LED, HIGH);
-  delay(500);
+  if (programButtons()) {
+    digitalWrite(LIVE_LED, LOW);
+    logLine("Serial?", 10);
+    int c = 100;
+    Serial.begin(9600);
+    while (!Serial) {
+      delay(10);
+      c--;
+      if (c <= 0) {
+        logLine("Timeout", 10);
+        break;
+      }
+    }
+    if (c > 0) {
+      logLine("Serial OK", 10);
+    }
+    delay(1000);
+    logLine("FS Init", 10);
+    if (!FatFS.begin()) {
+      logLine("FS Fail", 10);
+    } else {
+      logLine("FS OK", 10);
+    }
+    FatFSUSB.onUnplug(unplug);
+    FatFSUSB.onPlug(plug);
+    FatFSUSB.driveReady(mountable);
+    // Start FatFS USB drive mode
+    FatFSUSB.begin();
+    while (programButtons()) {
+      delay(500);
+    }
+    logLine("Program", 1000);
+    setMode(MODE_PGM);
+  } else {
+    loadKeyData();
+    setMode(MODE_HID);
+  }
+  digitalWrite(LIVE_LED, HIGH);
   updateScreen = true;
   buttonBits = 0;
 }
